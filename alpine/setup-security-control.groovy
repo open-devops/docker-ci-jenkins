74883b00fbb10b5000a294c017e2d17f1868d869
Songhao Quan <songhao.quan@icloud.com>
2016-08-24 14:00:27 +0800

    make jenkins admin user credential as env variable

--- alpine/setup-security-control.groovy
+++ alpine/setup-security-control.groovy
@@ -4,13 +4,16 @@ import jenkins.model.*
 
 def instance = Jenkins.getInstance()
 
+def adminUID = System.getenv("JENKINS_ADMIN_USER")
+def adminPWD = System.getenv("JENKINS_ADMIN_PASSWORD")
+
 println "--> Checking if security has been set already"
 
 if (!instance.isUseSecurity()) {
-    println "--> creating local user 'admin'"
+    println "--> creating jenkins local admin user"
 
     def hudsonRealm = new HudsonPrivateSecurityRealm(false)
-    hudsonRealm.createAccount('admin', 'admin')
+    hudsonRealm.createAccount(adminUID, adminPWD)
     instance.setSecurityRealm(hudsonRealm)
 
     def strategy = new FullControlOnceLoggedInAuthorizationStrategy()
